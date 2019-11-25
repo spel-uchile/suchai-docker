74b1f1a8a4e2e7511a09a35e2dd4003a63ec0ac6
crojas <camrojas@uchile.cl>
2019-11-25 12:05:56 -0300

    edit install scripts for linux and test

--- install_build_linux.sh
+++ install_build_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd SUCHAI-Flight-Software
 git pull
-python3 compile.py LINUX LINUX --comm 1 --fp 1 --hk 1 --test 0 --st_mode 1 --drivers
+python3 compile.py LINUX X86 --comm 1 --fp 1 --hk 1 --test 0 --st_mode 1 --drivers
