28468030396795a23d6aa8494cd6ae49eab8cb68
Carlos Gonzalez <contacto.carlosgonzalez@gmail.com>
2020-02-25 12:35:00 -0300

    Fix condition (again)

--- install_test_linux.sh
+++ install_test_linux.sh
@@ -3,7 +3,7 @@ cd SUCHAI-Flight-Software
 git pull
 test=$1
 echo $test
-if [ "$test" == "test_cmd" ]
+if [ "$test" = "test_cmd" ]
 then
     python3 compile.py LINUX X86 --comm 0 --fp 0 --hk 0 --st_mode 1 --drivers --test 1 --test_type $test
 else
